7c0fda8c7b2adcde07f775c39a15088e626780e3
Victor Mariano <102543880+VMarianOo@users.noreply.github.com>
2023-11-24 13:48:06 -0300

    Projeto Estacionamento Refeito

---
+++ DWIII/ProjetoEstacionamento/src/main/java/br/com/aula/projetoestacionamento/service/UsuarioService.java
@@ -0,0 +1,42 @@
+package br.com.aula.projetoestacionamento.service;
+
+import br.com.aula.projetoestacionamento.entity.Usuario;
+import br.com.aula.projetoestacionamento.repository.UsuarioRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id){
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("#### USUARIO NÃO ENCONTRADO #####")
+        );
+    }
+
+    @Transactional
+    public Usuario editarSenha(long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
+}
