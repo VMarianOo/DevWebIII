53215f681fe95376aedfb3748bac474b6a7541bc
Victor Mariano <102543880+VMarianOo@users.noreply.github.com>
2023-11-24 13:43:47 -0300

    Delete DWIII/ProjetoEstacionamento directory
    Deletando repositório para organização

--- DWIII/ProjetoEstacionamento/src/main/java/br/com/aula/projetoestacionamento/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.aula.projetoestacionamento.repository;
-
-import br.com.aula.projetoestacionamento.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
